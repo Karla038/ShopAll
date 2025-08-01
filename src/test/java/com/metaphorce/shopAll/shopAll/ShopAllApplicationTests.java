9c81003bf2aeeb3c8ed1337a4c109955042de16e
Karla038 <karladk038@gmail.com>
2025-08-01 14:46:20 -0600

    Inicio de proyecto

---
+++ src/test/java/com/metaphorce/shopAll/shopAll/ShopAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaphorce.shopAll.shopAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
